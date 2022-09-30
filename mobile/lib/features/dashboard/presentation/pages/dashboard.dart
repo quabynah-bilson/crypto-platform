d8760ea021ee8befbd36d9e251541e83ff84dc66
quabynah-bilson <84394923+quabynah-bilson@users.noreply.github.com>
2022-09-30 01:39:42 +0000

    Update dashboard.dart

--- mobile/lib/features/dashboard/presentation/pages/dashboard.dart
+++ mobile/lib/features/dashboard/presentation/pages/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fluttericon/font_awesome5_icons.dart';
 import 'package:lottie/lottie.dart';
 import 'package:mobile/features/shared/presentation/manager/crypto_cubit.dart';
 import 'package:mobile/features/shared/presentation/widgets/crypto.wallet.card.dart';
@@ -18,7 +19,7 @@ class _DashboardPageState extends State<DashboardPage> {
 
   @override
   Widget build(BuildContext context) {
-    kUseDefaultOverlays(context);
+    kUseDefaultOverlays(context, statusBarBrightness: Brightness.light);
 
     return MultiBlocListener(
       listeners: [
@@ -44,7 +45,7 @@ class _DashboardPageState extends State<DashboardPage> {
       child: Scaffold(
         body: CustomScrollView(
           shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
+          // physics: const NeverScrollableScrollPhysics(),
           slivers: [
             /// header bar (logo, title, user profile image)
             SliverToBoxAdapter(
@@ -84,9 +85,7 @@ class _DashboardPageState extends State<DashboardPage> {
                                   width: 2.5),
                             ),
                             // todo => replace with user profile image
-                            child:
-                               Samples.kSampleUserAvatar
-                                    .avatar(
+                            child: Samples.kSampleUserAvatar.avatar(
                               size: 56,
                               circular: true,
                             ),
@@ -105,9 +104,63 @@ class _DashboardPageState extends State<DashboardPage> {
               padding: EdgeInsets.fromLTRB(16, 16, 16, 24),
               sliver: SliverToBoxAdapter(child: CryptoWalletCard()),
             ),
+
+            /// actions
+            SliverToBoxAdapter(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  _buildActionButton('Send', FeatherIcons.send),
+                  _buildActionButton(
+                      'Receive', FeatherIconsSnakeCase.phone_incoming),
+                  _buildActionButton('Swap', Icons.swap_calls),
+                  _buildActionButton('Buy', FontAwesome5.wallet),
+                ],
+              ),
+            ),
+
+            /// recent transactions
+            SliverPadding(
+              padding: const EdgeInsets.only(top: 24),
+              sliver: SliverFillRemaining(
+                child: Container(
+                  // todo => build recent transactions
+                  color: context.colorScheme.background,
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
+
+  /// build action button
+  Widget _buildActionButton(String title, IconData icon,
+          [void Function()? onTap]) =>
+      GestureDetector(
+        onTap: onTap ??
+            () => context.showCustomDialog(
+                headerIconAsset: kAppLogo, message: kFeatureUnderDev),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: kToolbarHeight,
+              width: kToolbarHeight,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: context.colorScheme.surface,
+                border:
+                    Border.all(color: context.theme.disabledColor, width: 1.5),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Icon(icon, color: context.colorScheme.primary),
+            ),
+            title.button(context).top(8),
+          ],
+        ),
+      );
 }
